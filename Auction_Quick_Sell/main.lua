f530166b5b8ad4c0c410080d34939ab3ec84584e
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2023-01-03 16:13:14 +0800

    Add files via upload
    added colour too some of the commands and commented the code

--- Auction_Quick_Sell/main.lua
+++ Auction_Quick_Sell/main.lua
@@ -9,7 +9,7 @@ local green = 2;
 local blue = 3;
 local purple = 4;
 local orange = 5;
-local name, texture, count, quality, canUse, vendor, pricePerUnit, stackCount, totalCount -- stores info from fetchs
+local name,quality,vendor,stackCount, totalCount -- stores info from fetchs
 local bid
 local bo
 local LE_Button
@@ -27,7 +27,7 @@ local LE_Button_X = (0);
 local LE_Button_Y = (0);
 local LE_Button_Parent = (UIParent);
 
-function LE_Button_New()
+function LE_Button_New() -- Creates a Button for manual users
 	LE_Button = CreateFrame("Button",LE_Button_Name,LE_Button_Parent,LE_Button_Inherit);
 	LE_Button:SetWidth(LE_Button_Width);
 	LE_Button:SetHeight(LE_Button_Height);
@@ -39,7 +39,7 @@ end
 
 
 
-function Prep_item()
+function Prep_item() -- prepares the bid and bo values for items by multipliying them by the various salts and down scales
     if quality == grey then
         bid = math.floor(vendor * AucQS_settings_live.grey_salt * AucQS_settings_live.down_scale_bid)
         bo = math.floor(vendor * AucQS_settings_live.grey_salt * AucQS_settings_live.down_scale_BO)
@@ -73,11 +73,11 @@ function Prep_item()
 end
 
 
-local function auctionMaker5000()
+local function auctionMaker5000() -- creates the auctions
     local money = GetMoney()
     local Deposit_Cost = 0
-    if bid == bo then return end
-    if AucQS_settings_live.sell_all == false then
+    if bid == bo then return end -- to catch items that can't be sold or have zero value
+    if AucQS_settings_live.sell_all == false then -- standard auction if only selling one item at a time
         Deposit_Cost = math.floor(vendor * ah_faction * 1 * 1) * runtime
         if money < Deposit_Cost then
             print("Sorry not enough money for the deposit")
@@ -86,7 +86,7 @@ local function auctionMaker5000()
             return
         end
         StartAuction(bid,bo,runtime,1,1);
-    elseif AucQS_settings_live.sell_all == true and stackCount == 1 then
+    elseif AucQS_settings_live.sell_all == true and stackCount == 1 then -- sells multiple stacks of one ie having 4 of the same armor piece
         Deposit_Cost = math.floor(vendor * ah_faction * 1 * totalCount) * runtime
         if money < Deposit_Cost then
             print("Sorry not enough money for the deposit")
@@ -95,7 +95,7 @@ local function auctionMaker5000()
             return
         end
         StartAuction(bid,bo,runtime,1,totalCount);
-    elseif AucQS_settings_live.sell_all == true and stackCount ~= 1 and totalCount < stackCount then
+    elseif AucQS_settings_live.sell_all == true and stackCount ~= 1 and totalCount < stackCount then -- sells a stack of an amount less than max stack size
         Deposit_Cost = math.floor(vendor * ah_faction * totalCount * 1) * runtime
         if money < Deposit_Cost then
             print("Sorry not enough money for the deposit")
@@ -104,7 +104,7 @@ local function auctionMaker5000()
             return
         end
         StartAuction(bid,bo,runtime,totalCount,1);
-    elseif AucQS_settings_live.sell_all == true and stackCount ~= 1 then
+    elseif AucQS_settings_live.sell_all == true and stackCount ~= 1 then -- sells multiple stacks of full size
         local total_stacks = math.floor(totalCount/stackCount)
         Deposit_Cost = math.floor(vendor * ah_faction * totalCount * total_stacks) * runtime
         if money < Deposit_Cost then
@@ -115,35 +115,35 @@ local function auctionMaker5000()
         end
         StartAuction(bid,bo,runtime,stackCount,total_stacks);
     end
-    bid = 1
+    bid = 1 -- to reset the values for next time
     bo = 1
 end
 
-function LE_Button_OnClick()
+function LE_Button_OnClick() --handles the button clicking auto mode is turned off
     if name == nil then return end
     Prep_item()
     auctionMaker5000()
 end
 
 
- function events: NEW_AUCTION_UPDATE(self, event, ...)
-    name, texture, count, quality, canUse, vendor, pricePerUnit, stackCount, totalCount = GetAuctionSellItemInfo();
+ function events: NEW_AUCTION_UPDATE(...) -- used to automatically create auctions by forcing the currently selected item through the auctionmaker
+    name,quality, vendor, stackCount, totalCount = GetAuctionSellItemInfo();
     if name == nil then return end
     if AucQS_settings_live.is_auto == true then
         Prep_item();
         auctionMaker5000()
         end
     end
-function events: AUCTION_HOUSE_SHOW(...)
+function events: AUCTION_HOUSE_SHOW(...) -- if the auction house is opened with is auto off will create a button
     if AucQS_settings_live.is_auto == false then LE_Button_New();
     end
 end
-function events: AUCTION_HOUSE_CLOSED(...)
+function events: AUCTION_HOUSE_CLOSED(...) -- purges the button if it exists on close
     if LE_Button == nil then return end
     LE_Button:Hide();
  end
 
-function events: ADDON_LOADED(arg1,...)
+function events: ADDON_LOADED(arg1,...) -- Controls boot for Saved variable purposes
     if AucQS_settings == nil and arg1 == "Auction_Quick_Sell" then  -- for first time loader
         AucQS_settings_live = {
             is_auto = false,
@@ -157,7 +157,7 @@ function events: ADDON_LOADED(arg1,...)
             purple_salt = 40,
             orange_salt = 50,
         }
-    elseif arg1 == "Auction_Quick_Sell" and AucQS_settings ~= nil then
+    elseif arg1 == "Auction_Quick_Sell" and AucQS_settings ~= nil then -- regular settings updater
         AucQS_settings_live = AucQS_settings
     end
 end
@@ -165,7 +165,7 @@ function events: PLAYER_ENTERING_WORLD(...)
 
 end
 
-function events: PLAYER_LEAVING_WORLD(...)
+function events: PLAYER_LEAVING_WORLD(...) -- saves settings when the user terminates connection to the world
     AucQS_settings = AucQS_settings_live
 end
 
@@ -185,7 +185,7 @@ SLASH_AQSgrey1 = "/AucQSGrey"
 SLASH_AQSgrey2 = "/aucqsGrey"
 SLASH_AQSgrey3 = "/AUCQSGrey"
 
-function SlashCmdList.AQS(msg)
+function SlashCmdList.AQS(msg) -- handles all the chat functions for the addon, I should probably build a GUI for this but i like chat commands and don't like LUA very much
     msg = string.upper(msg)
     local args = {}
     local count = 0;
@@ -196,7 +196,7 @@ function SlashCmdList.AQS(msg)
     end
     if args[1] == "HELP" then
         print("list of commands")
-        print("Current values")
+        print("Currentvalues")
         print("Displays all user definable values")
         print("Autochange")
         print("This changes the autosell mode")
@@ -213,12 +213,12 @@ function SlashCmdList.AQS(msg)
         print("Auto stack sell",AucQS_settings_live.sell_all)
         print("Bid Downscale percentage",AucQS_settings_live.down_scale_bid * 100, "%")
         print("Buyout Downscale percentage",AucQS_settings_live.down_scale_BO * 100, "%")
-        print("Grey Multiplier",AucQS_settings_live.grey_salt) 
-        print("White Multiplier",AucQS_settings_live.white_salt)
-        print("Green Multiplier",AucQS_settings_live.green_salt)
-        print("Rare Multiplier",AucQS_settings_live.blue_salt)
-        print("Purple Multiplier",AucQS_settings_live.purple_salt)
-        print("Orange Multiplier",AucQS_settings_live.orange_salt)
+        print("\124cff888888Grey Multiplier\124r",AucQS_settings_live.grey_salt) 
+        print("\124cffffffffWhite Multiplier\124r",AucQS_settings_live.white_salt)
+        print("\124cff00ff00Green Multiplier\124r",AucQS_settings_live.green_salt)
+        print("\124cFF0070ddRare Multiplier\124r",AucQS_settings_live.blue_salt)
+        print("\124cFFa335eePurple Multiplier\124r",AucQS_settings_live.purple_salt) --a335ee 8A2BE2
+        print("\124cffff8000Orange Multiplier\124r",AucQS_settings_live.orange_salt)
     elseif args[1] == "BIDUNDER" then
         if count > 1 then
             local new_down_under_scale_bid = tonumber(args[2])
@@ -245,7 +245,7 @@ function SlashCmdList.AQS(msg)
             end
         end
         print("Buyout undercut", AucQS_settings_live.down_scale_BO*100, "%")
-    elseif args[1] == "GREY" then
+    elseif args[1] == "GREY" or args[1] == "GRAY" then
         if count  > 1 then
             local new_salt = tonumber(args[2])
             if new_salt == nil then
@@ -256,7 +256,7 @@ function SlashCmdList.AQS(msg)
                 AucQS_settings_live.grey_salt = new_salt
             end
         end
-        print("Grey multiplier")
+        print("\124cff888888Grey multiplier\124r")
         print(AucQS_settings_live.grey_salt)
     elseif args[1] == "WHITE" then
         if count  > 1 then
@@ -269,7 +269,7 @@ function SlashCmdList.AQS(msg)
                 AucQS_settings_live.white_salt = new_salt
             end
         end
-        print("White multiplier")
+        print("\124cffffffffWhite multiplier\124r")
         print(AucQS_settings_live.white_salt)
     elseif args[1] == "GREEN" then
         if count  > 1 then
@@ -282,7 +282,7 @@ function SlashCmdList.AQS(msg)
                 AucQS_settings_live.green_salt = new_salt
             end
         end
-        print("Green multiplier")
+        print("\124cff00ff00Green multiplier\124r")
         print(AucQS_settings_live.green_salt)
     elseif args[1] == "BLUE" then
         if count  > 1 then
@@ -295,7 +295,7 @@ function SlashCmdList.AQS(msg)
                 AucQS_settings_live.blue_salt = new_salt
             end
         end
-        print("Blue multiplier")
+        print("\124cFF0070ddBlue multiplier\124r")
         print(AucQS_settings_live.blue_salt)
     elseif args[1] == "PURPLE" then
         if count  > 1 then
@@ -308,7 +308,7 @@ function SlashCmdList.AQS(msg)
                 AucQS_settings_live.purple_salt = new_salt
             end
         end
-        print("Purple multiplier")
+        print("\124cFFa335eePurple multiplier\124r")
         print(AucQS_settings_live.purple_salt)
     elseif args[1] == "ORANGE" then
         if count  > 1 then
@@ -321,7 +321,7 @@ function SlashCmdList.AQS(msg)
                 AucQS_settings_live.orange_salt = new_salt
             end
         end
-        print("orange multiplier")
+        print("\124cffff8000Orange multiplier\124r")
         print(AucQS_settings_live.orange_salt)
     elseif args[1] == "AUTOCHANGE" then
         AucQS_settings_live.is_auto = not AucQS_settings_live.is_auto
